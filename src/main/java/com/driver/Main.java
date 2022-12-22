914b11d9011aa92a9cdb48341e18ffddcac6d0c4
Shubham Singh <shubhamsin.464@gmail.com>
2022-12-22 10:07:37 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,7 +10,7 @@ public class Main {
      }
      public static class B extends  A{
          @Override
-         String meth(){
+         public String meth(){
              return "Method is overridden in Extendend class B";
          }
     }
